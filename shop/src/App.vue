ea108d7ca66fb95702052d93fb0e881c4e05f468
belkud <vlad-belkud@yandex.ru>
2024-02-21 12:39:02 +0300

    adfs

--- shop/src/App.vue
+++ shop/src/App.vue
@@ -1,47 +1,40 @@
-<script setup lang="ts">
-import HelloWorld from './components/HelloWorld.vue'
-import TheWelcome from './components/TheWelcome.vue'
-</script>
+
+
 
 <template>
-  <header>
-    <img alt="Vue logo" class="logo" src="./assets/logo.svg" width="125" height="125" />
-
-    <div class="wrapper">
-      <HelloWorld msg="You did it!" />
-    </div>
-  </header>
-
-  <main>
-    <TheWelcome />
-  </main>
+   <div >{{ info }}</div>
+   <div >{{ someInfo }}</div>
+    <!-- <button type="button" @click="like">количество лайков: {{ like++ }}</button> -->
+  <button type="button" @mousemove="this.info= 'что-то новенькое'"> Провести мышкой</button>
+  <button type="button" @click="this.info= 'что-то новенькое'"> Нажать по клику</button>
 </template>
 
+  <script  lang="ts">
+  export default {
+      data() {
+          return {
+            info:'Title' as any,
+            someInfo:'Message'
+          }
+      },
+      methods: {
+        
+      }
+  }
+  let like = 0
+  </script>
+
+
+
+
 <style scoped>
-header {
-  line-height: 1.5;
-}
-
-.logo {
-  display: block;
-  margin: 0 auto 2rem;
-}
-
-@media (min-width: 1024px) {
-  header {
-    display: flex;
-    place-items: center;
-    padding-right: calc(var(--section-gap) / 2);
+  button {
+    color: aliceblue;
+    padding: 5px;
+    margin-top: 10px;
+    background-color: crimson;
   }
-
-  .logo {
-    margin: 0 2rem 0 0;
-  }
-
-  header .wrapper {
-    display: flex;
-    place-items: flex-start;
-    flex-wrap: wrap;
-  }
-}
+ 
+  
 </style>
+ 
