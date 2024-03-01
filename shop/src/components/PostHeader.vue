1156762a063f25715042195308423b7d5e526236
belkud <vlad-belkud@yandex.ru>
2024-03-01 14:39:26 +0300

    s

--- shop/src/components/PostHeader.vue
+++ shop/src/components/PostHeader.vue
@@ -4,13 +4,13 @@
 <!-- 2. Возможно помочь со splice в PostEntrance  -->
 
 <template>
-    <div class="text-3xl font-bold underline">
+    <div class="text-3xl font-bold bg-red-200">
         dafdf
     </div>
-
+<div className="text-3xl font-bold bg-red-200">kjh</div>
 </template>
 
-<script>
+<script lang="ts">
 
 </script>
 
--- shop/tailwind.config.js
+++ shop/tailwind.config.js
@@ -2,11 +2,13 @@
 export default {
   content: [
     "./index.html",
+    // "./components/PostHeader.vue",
     "./src/**/*.{vue,js,ts,jsx,tsx}",
   ],
   theme: {
     extend: {},
   },
   plugins: [],
+  // tailwind.config.{js,cjs,mjs,ts}
 }
 
