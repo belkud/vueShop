013869b8c82cac50393b127cc6012316b4015c2b
belkud <vlad-belkud@yandex.ru>
2024-03-01 17:52:35 +0300

    finish

--- shop/src/components/PostHeader.vue
+++ shop/src/components/PostHeader.vue
@@ -4,13 +4,13 @@
 <!-- 2. Возможно помочь со splice в PostEntrance  -->
 
 <template>
-    <div class=" flex text-xl gap-3 mt-5">
-        <div>Каталог &#128366;</div>
-        <input placeholder="&#128269; Поиск по сайту" class=" rounded-sm w-2/5" >
-        <div>Заказы 	&#128504;</div>
-        <div>Избранное &#9829;</div>
-        <div>Корзина &#128722</div>
-        <div>Войти &#128274;</div>
+    <div class=" cursor-pointer flex text-xl gap-24 mt-5 w-full ml-14">
+        <div class=" cursor-pointer">Каталог &#128366;</div>
+            <input placeholder="&#128269; Поиск по сайту" class=" rounded-sm w-2/5" >
+                <div class=" cursor-pointer">Заказы 	&#128504;</div>
+                <div class=" cursor-pointer">Избранное &#9829;</div>
+            <div class=" cursor-pointer">Корзина &#128722</div>
+        <div class=" cursor-pointer">Войти &#128274;</div>
         <!-- &#128275;	Замок открыт -->
         <!-- text-3xl mt-3 p-5  text-3xl font-bold -->
     </div>
