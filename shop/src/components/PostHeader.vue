5fe8918a2c33a51723ae0bf0efb98357a4fce00d
belkud <vlad-belkud@yandex.ru>
2024-03-01 17:21:05 +0300

    PostHeader

--- shop/src/components/PostHeader.vue
+++ shop/src/components/PostHeader.vue
@@ -4,10 +4,16 @@
 <!-- 2. Возможно помочь со splice в PostEntrance  -->
 
 <template>
-    <div class="text-3xl font-bold bg-red-200 ">
-        dafdf
+    <div class=" flex text-xl gap-3">
+        <div class="">Каталог &#128366;</div>
+        <input placeholder="&#128269; Поиск по сайту" class=" place-content-center" >
+        <div>Заказы 	&#128504;</div>
+        <div>Избранное &#9829;</div>
+        <div>Корзина &#128722</div>
+        <div>Войти &#128274;</div>
+        <!-- &#128275;	Замок открыт -->
+        <!-- text-3xl mt-3 p-5  text-3xl font-bold -->
     </div>
-<div class="text-3xl mt-3 p-5">kjh</div>
 </template>
 
 <script lang="ts">
