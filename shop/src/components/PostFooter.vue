f346bd1b68b749e277a7d7b3f5e64d61f8437a9f
belkud <vlad-belkud@yandex.ru>
2024-03-03 14:57:45 +0300

    Footer

--- shop/src/components/PostFooter.vue
+++ shop/src/components/PostFooter.vue
@@ -1,15 +1,19 @@
 
 <template >
     <footer>
-        <div class="flex bg-slate-800 mt-8 gap-16 p-3">
-            <div>Ozon Job</div>
-            <div>IT</div>
-            <div>Офис</div>
-            <div>Финтех</div>
-            <div>Fresh</div>
-            <div>Логистика</div>
-            <div>Работа на складе</div>
-            <div>События</div>
+        <div class="flex bg-slate-800 mt-8 px-5 py-3 gap-32">
+            <!-- <div class=" cursor-pointer hover:text-red-600">Работа</div> -->
+            <div class=" cursor-pointer hover:text-red-600">Офис</div>
+            <div class=" cursor-pointer hover:text-red-600">Вакансии</div>
+            <div class=" cursor-pointer hover:text-red-600">IT</div>
+            <div class=" cursor-pointer hover:text-red-600">Партнёры</div>
+            <div class=" cursor-pointer hover:text-red-600">Финтех</div>
+            <div class=" cursor-pointer hover:text-red-600">События</div>
+            <div class=" cursor-pointer hover:text-red-600">Оплата</div>
+            <div class=" cursor-pointer hover:text-red-600">Fresh</div>
+            <div class=" cursor-pointer hover:text-red-600">Логистика</div>
+            <div class=" cursor-pointer hover:text-red-600">Безопасность</div>
+            <div class=" cursor-pointer hover:text-red-600">Другое</div>
         </div>
 
     </footer>
--- shop/src/components/PostHeader.vue
+++ shop/src/components/PostHeader.vue
@@ -4,14 +4,16 @@
 <!-- 2. Возможно помочь со splice в PostEntrance  -->
 
 <template>
-    <div class=" flex pl-8 text-xl gap-16 mt-5 w-full">
-        <div class=" cursor-pointer" @click="conFunc">Каталог &#128366;</div>
+    <div class=" flex pl-8  gap-16 mt-5 w-full">
+        <div class=" cursor-pointer hover:text-red-600" @click="conFunc">Каталог &#128366;</div>
             <input placeholder="&#128269; Поиск по сайту" class=" border border-cyan-600 rounded-sm w-2/5 px-3 bg-gray-800"  >
-                <div class=" cursor-pointer">Заказы 	&#128504;</div>
-                <div class=" cursor-pointer">Избранное &#9829;</div>
-            <div class=" cursor-pointer">Корзина &#128722</div>
-        <div v-if="count%2==0" class=" cursor-pointer">Войти &#128274;</div>
-        <div v-if="count%2!=0" class=" cursor-pointer">В айти &#128275;</div>
+                <div class=" cursor-pointer hover:text-red-600">Заказы 	&#128504;</div>
+                <div class=" cursor-pointer hover:text-red-600">Избранное &#9829;</div>
+            <div class=" cursor-pointer hover:text-red-600">Корзина &#128722</div>
+            <div class=" cursor-pointer hover:text-red-600">Баланс карты &#8381</div>
+            <div v-if="count%2==0" class=" cursor-pointer hover:text-red-600">Войти &#128274;</div>
+            <div v-if="count%2!=0" class=" cursor-pointer hover:text-red-600">В айти &#128275;</div>
+            <div class=" cursor-pointer hover:text-red-600">Служба поддержки &#128222</div>
         <!-- text-3xl mt-3 p-5  text-3xl font-bold -->
     </div>
 
